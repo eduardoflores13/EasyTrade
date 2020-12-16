68a44902aec5e7082a01836eb9f9070da2871a1d
Jimmy Ventura <jimmy.ventura@ujcv.edu.hn>
2020-12-16 11:34:15 -0600

    Agregue 4 view, proximamente los intents y los recycle views funcionando.

---
+++ app/src/main/java/com/example/easytrade/aceptaroferta_activity.kt
@@ -0,0 +1,13 @@
+package com.example.easytrade
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class aceptaroferta_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+    }
+}
